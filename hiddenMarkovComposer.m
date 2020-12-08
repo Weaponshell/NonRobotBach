f7c2387cc98b640958fca10c544269b6499aaa01
Weaponshell <daan_opheikens@hotmail.com>
2020-12-08 13:35:39 +0100

    Added initial given files
    Files from the nestor page

---
+++ hiddenMarkovComposer.m
@@ -0,0 +1,7 @@
+function [soundvector] = hiddenMarkovComposer(inputArg1,inputArg2)
+%HIDDENMARKOVCOMPOSER Summary of this function goes here
+%   Detailed explanation goes here
+outputArg1 = inputArg1;
+outputArg2 = inputArg2;
+end
+
